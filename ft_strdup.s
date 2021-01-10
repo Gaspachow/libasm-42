404c86ec86b2a7a4d348c0664ffe34d2df0a8bd0
gsmets <gsmets@student.42.fr>
2021-01-10 13:37:58 +0100

    makefile + strdup and main tests

--- ft_strdup.s
+++ ft_strdup.s
@@ -8,16 +8,16 @@ ft_strdup:					; strdup receives src string's address as rdi
 
 	push	rdi				; saving the value of rdi to use later in strcpy
 	call	ft_strlen		; strlen takes rdi and returns the length in rax
-	inc		rax
-	mov		rdi, rax
-	call	malloc
-	cmp		rax, 0
-	je		fail
-	mov		rdi, rax
-	pop		rsi
-	call	ft_strcpy
-	ret
+	inc		rax				; increase rax by one to account for the '/0'
+	mov		rdi, rax		; move rax to rdi
+	call	malloc			; malloc takes rdi as argument for the size to allocate
+	cmp		rax, 0			; compare the rax (returned value of malloc) with NULL
+	je		fail			; if equal, malloc returned NULL and there was a fail
+	mov		rdi, rax		; we move the allocated memory address to rdi (first arg)
+	pop		rsi				; we pop the saved src address from the pile into rsi (second arg)
+	call	ft_strcpy		; ft_strcpy takes rdi and rsi as arguments and copies rsi in rdi
+	ret						; returns rax (which still has the destination address)
 
 fail:
-	mov		rax, 0
-	ret
+	mov		rax, 0			; if fail, strdup returns NULL so we move 0 in rax
+	ret						; returns rax (0)
--- main.c
+++ main.c
@@ -1,18 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsmets <gsmets@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/10 12:21:00 by gsmets            #+#    #+#             */
+/*   Updated: 2021/01/10 13:04:48 by gsmets           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <fcntl.h>
 
-
-int ft_write(int fd,char *str, int len);
+size_t  ft_strlen(const char *str);
+char	*ft_strcpy(char *dst, const char *src);
+int 	ft_strcmp(const char *s1, const char *s2);
+ssize_t	ft_write(int fd, const void *addresss, size_t size);
+ssize_t	ft_read(int fd, void *addresss, size_t size);
+char	*ft_strdup(const char *str);
 
 int main() {
-    char s1[50] = "wewe";
-    char s2[50] = "wewe";
+    char *str1 = "This is a string...\n";
+    char *str2 = "Gaspacho\n";
+    char *empty = "";
+    char *ok = "qoigio;rqo;iqrinqr'inerqh'pinqhr'inqhr'pqerna\n";
 
-    ft_write(1, "hi\n", 3);
+    printf("\n\n-------------------TEST FT_STRLEN--------------------------\n\n");
 
-    // int i = ft_strcmp(s1, s2);
+    printf("  real: %lu\nlibasm: %lu\n", strlen(str1), ft_strlen(str1));
+    printf("  real: %lu\nlibasm: %lu\n", strlen(empty), ft_strlen(empty));
+    printf("  real: %lu\nlibasm: %lu\n", strlen(ok), ft_strlen(ok));
 
-     printf("%s\n", strerror(errno));
-    return (0);
+    printf("\n\n-------------------TEST FT_STRCPY--------------------------\n\n");
+
+    char    empty1[50];
+    char    empty2[50];
+
+    printf("  real: %s\nlibasm: %s\n", strcpy(empty1, str1), ft_strcpy(empty2, str1));
+
+    
+    printf("\n\n-------------------TEST FT_STRCMP--------------------------\n\n");
+
+    printf("  real: %i\nlibasm: %i\n", strcmp(str1, str2), ft_strcmp(str1, str2));
+    printf("  real: %i\nlibasm: %i\n", strcmp(empty, str1), ft_strcmp(empty, str1));
+    printf("  real: %i\nlibasm: %i\n", strcmp(ok, str2), ft_strcmp(ok, str2));
+
+
+
+    printf("\n\n-------------------TEST FT_WRITE--------------------------\n\n");
+
+	printf("real return: %zi\n", write(1, str2, 9));
+	printf("libasm return: %zi\n", ft_write(1, str2, 9));
+	printf("\n");
+	printf("Real Error:%zi, %s\n", write(-7, str2, 7), strerror(errno));
+	printf("Libasm Error:%zi, %s\n", ft_write(-7, str2, 7), strerror(errno));
+
+
+    printf("\n\n-------------------TEST FT_READ--------------------------\n\n");
+
+    char *buffer1 = calloc(500, sizeof(char));
+    char *buffer2 = calloc (500, sizeof(char));
+
+    int fd = open("test", O_RDONLY);
+	printf("real return: %zi, %s\n", read(fd, buffer1, 5), buffer1);
+    fd = open("test", O_RDONLY);
+	printf("libasm return: %zi, %s\n", ft_read(fd, buffer2, 5), buffer2);
+	printf("\n");
+	printf("Real Error:%zi, %s\n", read(-7, buffer1, 7), strerror(errno));
+	printf("Libasm Error:%zi, %s\n", ft_read(-7, buffer2, 7), strerror(errno));
+
+    printf("\n\n-------------------TEST FT_STRDUP--------------------------\n\n");
+
+    printf("  real: %s\nlibasm: %s\n", strdup(str1), ft_strdup(str1));
+    printf("  real: %s\nlibasm: %s\n", strdup(empty), ft_strdup(empty));
+    printf("  real: %s\nlibasm: %s\n", strdup(ok), ft_strdup(ok));
+
 }
